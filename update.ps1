3ec99b1ec9b2358f72a2a7bead0aa35e9424ac98
Brannon McGraw <deadpixel@MacBook-Air.local>
2019-04-03 15:13:30 -0400

    first

---
+++ update.ps1
@@ -0,0 +1,2 @@
+PowerShell -WindowStyle hidden -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms');
+[System.Windows.Forms.MessageBox]::Show('Your computer is currently being scanned, please do not turn off the computer until all actions are fully completed', 'Information Security')}"
